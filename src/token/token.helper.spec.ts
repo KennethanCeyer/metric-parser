e2a7a949b685403ebb1382c75b256c6104fbfcac
kennethanCeyer <kennethan@nhpcw.com>
2018-03-28 05:51:19 +0900

    WIP: separate unit test cases

--- src/token/token.helper.spec.ts
+++ src/token/token.helper.spec.ts
@@ -2,24 +2,35 @@ import { expect } from 'chai';
 import { Token } from './token';
 import { TokenHelper } from './token.helper';
 
-describe('TokenHelper.getPrecedence(token)', () => {
-    it('should return 0 with addition, subtraction', () => {
+describe('TokenHelper.getPrecedence()', () => {
+    it('should return 0 with addition', () => {
         expect(TokenHelper.getPrecedence(Token.literal.Addition)).to.equal(0);
+    });
+
+    it('should return 0 with subtraction', () => {
         expect(TokenHelper.getPrecedence(Token.literal.Subtraction)).to.equal(0);
     });
 
-    it('should return 1 with multiplication, division', () => {
+    it('should return 1 with multiplication', () => {
         expect(TokenHelper.getPrecedence(Token.literal.Multiplication)).to.equal(1);
+    });
+    it('should return 1 with division', () => {
         expect(TokenHelper.getPrecedence(Token.literal.Division)).to.equal(1);
     });
 
-    it('should return 2 with mod, pow', () => {
+    it('should return 2 with mod', () => {
         expect(TokenHelper.getPrecedence(Token.literal.Mod)).to.equal(2);
+    });
+
+    it('should return 2 with pow', () => {
         expect(TokenHelper.getPrecedence(Token.literal.Pow)).to.equal(2);
     });
 
-    it('should return 3 with brackets', () => {
+    it('should return 3 with bracket open', () => {
         expect(TokenHelper.getPrecedence(Token.literal.BracketOpen)).to.equal(3);
+    });
+
+    it('should return 3 with bracket close', () => {
         expect(TokenHelper.getPrecedence(Token.literal.BracketClose)).to.equal(3);
     });
 });
