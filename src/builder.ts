e1911118f9d86834d84740ac745033999973cb70
kennethanCeyer <kennethan@nhpcw.com>
2018-03-27 21:34:11 +0900

    WIP: remove unused lines

--- src/builder.ts
+++ src/builder.ts
@@ -1,8 +1,6 @@
 import { FormulaData, FormulaParseData } from './types';
-import { ParserProcess } from './parser/parser.process.type';
 import { LoggerCode } from './logger/logger.code';
-import { LoggerHelper } from './logger/logger.helper';
-import { Operand, OperandItemValue, OperandUnitValue, OperandValue, TreeWrapper, TreeModel } from './tree.type';
+import { TreeModel } from './tree.type';
 import { ParserDefaultResult, ParserResult } from './parser/parser.result';
 import { BuilderHelper } from './builder.helper';
 import { ParserHelper } from './parser/parser.helper';
@@ -23,44 +21,6 @@ export class Builder {
             return this.unparse(this._formula as TreeModel);
     }
 
-    /*
-    parseTree(result: TreeWrapper, line = 0,  col = 0): ParserResult<(OperandItemValue | OperandUnitValue)[]> {
-        this.validateParseTree(result, line, col);
-
-        if (!BuilderHelper.isOperand(result))
-            return {
-                code: LoggerCode.Success,
-                result: [this.getOperandValue((result as Operand).value)]
-            };
-
-        var params = ['operand1', 'operator', 'operand2'];
-        for (var idx = 0; idx < params.length; idx++) {
-            var param = params[idx];
-            if (typeof result[param] === 'object') {
-                var result = _this.parseTree(result[param], col + idx);
-                if (result.status === false) {
-                    return result;
-                } else {
-                    formula = formula.concat(result.result);
-                    if (typeof result.operator !== 'undefined' && result.operator !== null && typeof result.operator !== 'undefined' && result.operator !== null) {
-                        if (this.getOperatorPriority(result.operator) < this.getOperatorPriority(result.operator) && this.getOperatorPriority(result.operator) !== -1) {
-                            formula.splice([formula.length - 3], 0, '(');
-                            formula.splice([formula.length], 0, ')');
-                        }
-                    }
-                }
-            } else {
-                formula.push(result[param]);
-            }
-        }
-
-        return {
-            status: true,
-            result: formula,
-            operator: depth > 0 ? result.operator : undefined
-        };
-    }
-    */
 
     parse(data: FormulaParseData, pos = 0): ParserResult<TreeModel> {
         const parserToken = new TokenAnalyzer(ParserHelper.getArray(data));
